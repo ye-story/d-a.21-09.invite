243ea70dc8f82f26996f83143f9d95d0d728f1dd
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2025-04-18 12:25:56 +0300

    init

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const main = document.querySelector('main');
 
 const img1 = document.getElementById('img1');
-const img2 = document.getElementById('img2');
-// const img3 = document.getElementById('img3');
-// const img4 = document.getElementById('img4');
+
 const imgWrappers = [...document.querySelectorAll('.img-wrapper')];
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -11,14 +9,11 @@ const id = urlParams.get('id');
 
 const baseImgPath = './img/guests/';
 
-if (+id && !isNaN(+id) && (+id <= 24) && (+id > 0)) { 
-  // img1.src = baseImgPath + `1_${id}.jpg`;
-  //img2.src = baseImgPath + `2_${id}.jpg`;
-  img3.src = baseImgPath + `3_${id}.jpg`;
-  // img4.src = baseImgPath + '4.jpg';
+if (+id && !isNaN(+id) && (+id <= 19) && (+id > 0)) { 
+ img1.src = baseImgPath + `${id}.webp`; 
 }
 else {
-  img3.src = './img/3.jpg';
+  img1.src = './img/1.jpg';
 }
 
 const imgs = [...document.querySelectorAll('.img-wrapper img')];
@@ -28,29 +23,3 @@ imgWrappers.forEach((imgWrapper) => {
     imgWrapper.style.display = 'none';
   }
 });
-
-imgW = 722;
-imgH = 1299;
-
-function changeSizeWindow() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  if (width < height) {
-    const ratio = width / imgW;
-    main.style.width = imgW * ratio + 'px';
-
-    imgWrappers.forEach(function (imgWrapper) {
-      imgWrapper.style.height = height + 'px';
-    });
-  } else {
-    const ratio = height / imgH;
-    main.style.width = imgW * ratio + 'px';
-
-    imgWrappers.forEach(function (imgWrapper) {
-      imgWrapper.style.height = height + 'px';
-    });
-  }
-}
-
-changeSizeWindow();
